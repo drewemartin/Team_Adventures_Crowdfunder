e27c811ce35e39697f38a1e02c53784369d16281
zhengweiyi <zhengweiyi@gmail.com>
2014-07-17 15:24:11 -0400

    generate routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
+
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
   root to: 'projects#index'
 
-  
-  resources :projects
-  resources :categories
+  resources :projects, :only =>[:new, :create, :index, :show]
+
+  resources :pledge, :only =>[:create]
+
+  resources :categories, :only =>[:index, :show]
+
+  resources :user, :only =>[:new,:create, :show, :edit,:update]
+
+  resources :user_sessions, :only=>[:new,:create, :destroy]
+	get 'login' => 'user_sessions#new', :as => :login
+	post 'logout' => 'user_sessions#destroy', :as => :logout
 
 end
