69d76d4f34f4579add011878895e888c12ef604e
zhengweiyi <zhengweiyi@gmail.com>
2014-08-07 16:14:20 -0400

    add default picture url for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
 
   scope :most_recent_five, -> { all.limit(5) }
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://ima.gs/ddd/444/36f/unknown-300x300.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
