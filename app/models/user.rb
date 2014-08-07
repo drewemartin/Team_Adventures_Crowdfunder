2f24a50a36eb384f271586d1a6173e0ce4ccc2eb
zhengweiyi <zhengweiyi@gmail.com>
2014-08-07 16:07:46 -0400

    add user photo using paperclip gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,8 @@ has_secure_password
   #validates :username, length: {minimum: 2}
   validates :email, :presence => true
   validates :email, :uniqueness => true
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
 
